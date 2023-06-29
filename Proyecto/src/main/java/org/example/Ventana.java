42537b77ec55bf869deaed05a62b1f7fdf45f8a8
vicen <vwiederhold2021@udec.cl>
2023-06-28 21:52:47 -0400

    se agrego imagenes

--- Proyecto/src/main/java/org/example/Ventana.java
+++ Proyecto/src/main/java/org/example/Ventana.java
@@ -1,48 +1,75 @@
 package org.example;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.io.File;
 
 public class Ventana extends JFrame {
     public Ventana() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setTitle("Reserva de Pasajes");
+        setTitle("Buses ETM");
         setPreferredSize(new Dimension(640, 480));
 
-        JPanel panel = new JPanel();
-        panel.setBackground(Color.BLACK);
-        panel.setLayout(new GridLayout(2, 1)); // 2 filas, 1 columna
 
-        JButton boton1 = new JButton("Reservar");
-        JButton boton2 = new JButton("Ver Boletos Disponibles");
+        String imagePath = "C:\\Users\\vicen\\Desktop\\Proyecto-\\Imagenes\\Buses.jpg";
 
-        boton1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
+        File imageFile = new File(imagePath);
+        if (!imageFile.exists()) {
+            System.out.println("La imagen de fondo no existe");
+        } else {
+
+            ImageIcon backgroundImage = new ImageIcon(imagePath);
+
+            JLabel backgroundLabel = new JLabel(backgroundImage);
+
+            JPanel panel = new JPanel();
+            panel.setLayout(new BorderLayout());
+
+            panel.add(backgroundLabel, BorderLayout.CENTER);
+
+
+            JLabel reservaLabel = new JLabel("Reserva YA!");
+            reservaLabel.setFont(new Font("Arial", Font.BOLD, 24));
+            reservaLabel.setForeground(Color.BLACK);
+
+
+            JPanel topPanel = new JPanel();
+            topPanel.setOpaque(false);
+
+            topPanel.add(reservaLabel);
+
+            panel.add(topPanel, BorderLayout.NORTH);
+
+
+            JButton boton1 = new JButton("Empezar Reserva");
+
+
+            boton1.setPreferredSize(new Dimension(200, 100));
+
+
+            boton1.addActionListener(e -> {
                 SeleccionarOrigen seleccionarLugar = new SeleccionarOrigen();
                 seleccionarLugar.setVisible(true);
-                dispose(); // Cierra la ventana actual
+            });
 
-            }
-        });
 
-        boton2.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                SeleccionarAsientos reservarPasajes = new SeleccionarAsientos();
-                reservarPasajes.setVisible(true);
-                dispose(); // Cierra la ventana actual
-            }
-        });
+            JPanel buttonPanel = new JPanel();
+            buttonPanel.setLayout(new FlowLayout());
+            buttonPanel.setOpaque(false);
 
-        panel.add(boton1);
-        panel.add(boton2);
 
-        add(panel);
+            buttonPanel.add(boton1);
+
+            panel.add(buttonPanel, BorderLayout.SOUTH);
+
+            add(panel);
+        }
+
         pack();
         setLocationRelativeTo(null);
+        setVisible(true);
+    }
 
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(Ventana::new);
     }
 }
-
