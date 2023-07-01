9386034daf961ada6a4d5cb5ca6b78ac2b550d6b
franco-ponce <fraponce2021@udec.cl>
2023-07-01 04:40:10 -0400

    Se añadio los test para la clase Boleto

--- Proyecto/src/test/java/org/example/BoletoTest.java
+++ Proyecto/src/test/java/org/example/BoletoTest.java
@@ -10,5 +10,34 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class BoletoTest {
-
+    private Boleto b;
+    @BeforeEach
+    public void setUp(){
+        b = new Boleto("Concepcion","Santiago","Lunes",7,13);
+    }
+    @Test
+    @DisplayName("Test getDestino")
+    public void TestGetDestino() throws Exception{
+        assertEquals("Santiago",b.getDestino());
+    }
+    @Test
+    @DisplayName("Test getOrigenestino")
+    public void TestGetOrigen() throws Exception{
+        assertEquals("Concepcion",b.getOrigen());
+    }
+    @Test
+    @DisplayName("Test getDia")
+    public void TestGetDia() throws Exception{
+        assertEquals("Lunes",b.getDia());
+    }
+    @Test
+    @DisplayName("Test getSalida")
+    public void TestGetSalida() throws Exception{
+        assertEquals(7,b.getSalida());
+    }
+    @Test
+    @DisplayName("Test getDestino")
+    public void TestGetLlegada() throws Exception{
+        assertEquals(13,b.getLlegada());
+    }
 }
